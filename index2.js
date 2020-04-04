c4843f84d857d3d9a3dd8996e47f95ec83d8c1e8
henil0604 <62794871+henil0604@users.noreply.github.com>
2020-04-04 14:13:25 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -8,7 +8,7 @@ setInterval(() => {
 function updatePage() {
 
     console.log("Details Reloded")
-    fetch("https://henil0604.github.io/worldCorona.github.io/data.json);
+    fetch("https://henil0604.github.io/worldCorona.github.io/data.json")
         .then(response => response.json())
         .then(rsp => {
 
