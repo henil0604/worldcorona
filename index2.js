f05b86cac2ab25a467160c888014484ca2d950ba
henil0604 <62794871+henil0604@users.noreply.github.com>
2020-04-26 21:39:15 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -260,7 +260,7 @@ const countryList = [
 function updatePage() {
 
     console.log("Details Reloded")
-    fetch("http://127.0.0.1:5500/data.json")
+    fetch("https://henil0604.github.io/worldcorona.com/data.json")
         .then(response => response.json())
         .then(rsp => {
 
